f1d10d0aaa137a6ff9bcf7fa61c08f5222480786
johanari <johanari@student.matnat.uio.no>
2020-08-28 23:34:50 +0200

    Added more tests to test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,4 +1,7 @@
 import calculator
 def test_add():
     assert calculator.add(1,2)==3
-    
+def test_add2():
+    assert calculator.add(-1,-2)==-3
+def test_add3():
+    assert calculator.add(0.999,1.01)==2.009
