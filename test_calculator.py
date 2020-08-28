76e31a0edc4ea9b21f847b1a7e8484f30f3d81b2
johanari <johanari@student.matnat.uio.no>
2020-08-29 00:07:41 +0200

    Added a test for concatinating strings

--- test_calculator.py
+++ test_calculator.py
@@ -1,8 +1,11 @@
 import calculator
 def test_add(): #Checking if positive integers works
     assert calculator.add(1,2)==3
-def test_add2(): #Checking if negative integers works
     assert calculator.add(-1,-2)==-3 
-def test_add3(): #Checking if float works
+def test_add_float():    
     tol=10e-5 #Because of rounding errors, we need a tolerance
     assert abs(calculator.add(0.1,0.2)-0.3)<=tol
+def test_add_string():
+    string1="Hello "
+    string2="World"
+    assert calculator.add(string1,string2)=="Hello World"
