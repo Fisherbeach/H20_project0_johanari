a3f7f7df161308b0282d9b37f6b5e8967b40578b
johanari <johanari@student.matnat.uio.no>
2020-08-28 23:54:27 +0200

    Added a test for float

--- test_calculator.py
+++ test_calculator.py
@@ -1,7 +1,8 @@
 import calculator
-def test_add():
+def test_add(): #Checking if positive integers works
     assert calculator.add(1,2)==3
-def test_add2():
-    assert calculator.add(-1,-2)==-3
-def test_add3():
-    assert calculator.add(0.999,1.01)==2.009
+def test_add2(): #Checking if negative integers works
+    assert calculator.add(-1,-2)==-3 
+def test_add3(): #Checking if float works
+    tol=10e-5 #Because of rounding errors, we need a tolerance
+    assert abs(calculator.add(0.1,0.2)-0.3)<=tol
